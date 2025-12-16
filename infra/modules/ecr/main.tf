7d009e419eba6e090825f06741216110fd276961
Ashish16G <ashish.urmaliya16@gmail.com>
2025-12-17 01:19:44 +0530

    Add Jenkinsfile.destroy pipeline for safe infrastructure teardown and enable force_delete on ECR repositories

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -3,6 +3,7 @@ variable "repositories" { type = list(string) }
 resource "aws_ecr_repository" "this" {
   for_each = toset(var.repositories)
   name     = each.key
+  force_delete = true
   image_scanning_configuration { scan_on_push = true }
   encryption_configuration { encryption_type = "AES256" }
   tags = { Name = each.key }
